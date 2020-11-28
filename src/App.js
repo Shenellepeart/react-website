18791eed543fd99a44e0b9d5c6e1bf1bb56680b7
Shenelle_Peart <speart@limoanywhere.com>
2020-11-28 13:07:33 -0600

    Created layout for Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    <h1>wassuupp</h1>
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
 
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {NavLogo, NavbarContainer, Nav} from './NavbarElements';
+
+const Navbar = () => {
+    return (
+            <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo>
+                        dolla
+                    </NavLogo>
+                </NavbarContainer>
+            </Nav>
+            </>
+    )
+}
+
+export default Navbar;
