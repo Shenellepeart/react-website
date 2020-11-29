9bc6dacccce02897c093d29cd127c0c9e8d5b6a8
Shenelle_Peart <speart@limoanywhere.com>
2020-11-29 13:34:55 -0600

    Added Routes for Sign In Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages";
+import SignInPage from "./pages/SignIn";
 
 function App() {
   return (
     <Router>
-     <Home/>
+      <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/signIn" component={SignInPage} exact />
+      </Switch>
     </Router>
   );
 }
